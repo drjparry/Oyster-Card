b8ffaf3fa359d21532b5a821060ad0a6a21ee09e
Douglas Rose <douglahr@gmail.com>
2016-01-26 14:30:14 +0100

    enforcing maximum balance

--- lib/make_oystercard.rb
+++ lib/make_oystercard.rb
@@ -1,9 +1,12 @@
 class OysterCard
+  LIMIT = 50
 attr_reader :balance
   def initialize
     @balance = 0
   end
   def top_up(value)
+    fail "Max reached which is #{LIMIT}" if @balance + value > LIMIT
+
     @balance += value
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,7 +5,11 @@ describe OysterCard do
     expect(subject.balance).to eq 0
   end
   it "gets topped up "do
-    subject.top_up(65)
-    expect(subject.balance).to eq 65
+    subject.top_up(30)
+    expect(subject.balance).to eq 30
+  end
+  it "fails for a top-up more than 50" do
+    subject.top_up(50)
+    expect{subject.top_up(1)}.to raise_error "Max reached which is #{OysterCard::LIMIT}"
   end
 end
