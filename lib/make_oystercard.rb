6fb18eb3815cb19bf69ad504627538ecd83c69ef
Tom Pickard <tcpickard94@gmail.com>
2016-01-26 20:16:49 +0000

    Refactored code for readability and added named subject in test file

--- lib/make_oystercard.rb
+++ lib/make_oystercard.rb
@@ -1,42 +1,52 @@
 class OysterCard
   LIMIT = 50
   MINIMUM = 1
-attr_reader :balance, :entry_station, :exit_station, :journey_history
+  attr_reader :balance, :entry_station, :exit_station, :journey_history
+  
   def initialize
     @balance = 0
     @journey = false
     @entry_station = nil
     @journey_history = []
   end
+  
   def top_up(value)
     fail "Max reached which is #{LIMIT}" if maximum(value)
     @balance += value
   end
+  
   def in_journey?
     entry_station
   end
+  
   def touch_in(station)
     fail "Insufficient balance #{MINIMUM}" if minimum
     @exit_station = nil
     @entry_station = station
     record_the_journey(station)
   end
+  
   def touch_out(station)
     deduct(10)
     @entry_station = nil
     @exit_station = station
     record_the_journey(station)
   end
+  
   def record_the_journey(record)
     @journey_history << record
   end
+  
   private
+  
   def maximum(value)
     @balance + value > LIMIT
   end
+  
   def minimum
     @balance <= MINIMUM
   end
+  
   def deduct(value)
     @balance -= value
   end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,76 +1,81 @@
 require "make_oystercard.rb"
 describe OysterCard do
-
+  subject(:oystercard) {described_class.new}
+  
   it "Balance = 0" do
-    expect(subject.balance).to eq 0
+    expect(oystercard.balance).to eq 0
   end
 
   describe "#top_up" do
     before do
-      subject.top_up(30)
+      oystercard.top_up(30)
     end
+    
     it "gets topped up "do
-    expect(subject.balance).to eq 30
-  end
-  it "fails for a top-up more than 50" do
-    subject.top_up(20)
-    expect{subject.top_up(1)}.to raise_error "Max reached which is #{OysterCard::LIMIT}"
-  end
-end
-
-describe 'journey' do
-  before do
-    subject.top_up(10)
+      expect(oystercard.balance).to eq 30
+    end
+    
+    it "fails for a top-up more than 50" do
+      oystercard.top_up(20)
+      expect{oystercard.top_up(1)}.to raise_error "Max reached which is #{OysterCard::LIMIT}"
+    end
   end
 
-  let(:station) {double(:station)}
-  it 'journey commences' do
-    expect(subject).to_not be_in_journey
+  describe 'journey' do
+    let(:station) {double(:station)}
+    before do
+      oystercard.top_up(10)
+    end
+    
+    it 'journey commences' do
+      expect(oystercard).to_not be_in_journey
+    end
+    
+    context 'touch_in' do
+      it 'card has at least £1' do
+        oystercard.touch_out(station)
+        expect{oystercard.touch_in(station)}.to raise_error "Insufficient balance #{OysterCard::MINIMUM}"
+      end
+      
+      it 'taps-in' do
+        oystercard.touch_in(station)
+        expect(oystercard).to be_in_journey
+      end
+      
+      it 'has entry station' do
+        oystercard.touch_in(station)
+        expect(oystercard.entry_station).to eq station
+      end
+    end
+    
+    context 'touch_out' do
+
+      it 'taps-out' do
+        oystercard.touch_out(station)
+        expect(oystercard).to_not be_in_journey
+      end
+      
+      it 'charges on touch_out' do
+        expect{oystercard.touch_out(station)}.to change{oystercard.balance}.by(-10)
+      end
+      
+      it 'forgets entry station' do
+        oystercard.touch_in(station)
+        oystercard.touch_out(station)
+        expect(oystercard.entry_station).to eq nil
+      end
+
+      it 'remembers exit station' do
+        oystercard.touch_out(station)
+        expect(oystercard.exit_station).to eq station
+      end
+
+      it 'records journey history' do
+        oystercard.touch_in(station)
+        oystercard.touch_out(station)
+        expect(oystercard.journey_history).to eq [station,station]
+      end
+
+    end
   end
-  context 'touch_in' do
-    it 'card has at least £1' do
-      subject.touch_out(station)
-      expect{subject.touch_in(station)}.to raise_error "Insufficient balance #{OysterCard::MINIMUM}"
-    end
-    it 'taps-in' do
-      subject.touch_in(station)
-      expect(subject).to be_in_journey
-    end
-    it 'has entry station' do
-      subject.touch_in(station)
-      expect(subject.entry_station).to eq station
-  end
-  end
-  context 'touch_out' do
-
-    it 'taps-out' do
-      subject.touch_out(station)
-      expect(subject).to_not be_in_journey
-    end
-    it 'charges on touch_out' do
-      expect{subject.touch_out(station)}.to change{subject.balance}.by(-10)
-
-    end
-    it 'forgets entry station' do
-      subject.touch_in(station)
-      subject.touch_out(station)
-      expect(subject.entry_station).to eq nil
-    end
-
-    it 'remembers exit station' do
-      subject.touch_out(station)
-      expect(subject.exit_station).to eq station
-    end
-
-    it 'records journey history' do
-      subject.touch_in(station)
-      subject.touch_out(station)
-      expect(subject.journey_history).to eq [station,station]
-    end
-
-    # it 'records journey set' do
-    #   subject.journey_history
-    # end
-  end
-end
 end
